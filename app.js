270c64b066a881baa015ee062a6b282a14fa2d37
nikithesh-hr <nikitheshc@gmail.com>
2020-10-15 22:28:13 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+   
+   
+   server.route({
+    method: 'POST',
+    path: '/users',
+    handler: function (request, h) {
+
+        const payload = request.payload;
+
+        return `Client name :  ${payload.clientname}! , Date of meeting : ${payload.dateofmeeting}, Message : ${payload.message}`;
+    }
+   });
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
